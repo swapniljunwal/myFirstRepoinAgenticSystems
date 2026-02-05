d9ac353def959fc9474cbaad95410408bdb87ed2
swapniljunwal <swapniljunwal07@gmail.com>
2026-02-05 14:32:32 +0530

    Python code assignment

---
+++ python-fundamentals-assignment/python.py
@@ -0,0 +1,23 @@
+age = int(input("Enter Your Birth Year: "))
+
+Current_year = 2026
+
+your_age = Current_year - age
+
+print(f"You are {your_age} years old.")
+
+# ------------------x---------------
+
+user = float(input("Enter your number: "))
+
+print(f"Model accuracy is {user}")
+
+# ------------------x---------------
+
+user = input("Enter your name : ")
+
+age = int(input("Enter your age : "))
+
+active_user = True
+
+print(f"User {user} is {age} years old. Active status: {active_user}")
